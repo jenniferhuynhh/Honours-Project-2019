9ef663c70b3261513e989b178ba9b416ec59f14b
jennifer huynh <a1709507@student.adelaide.edu.au>
2019-05-21 23:38:23 +0930

    added display for weapon authorisation module. Closes #33

--- web_server/public/javascripts/weapon_authorisation_module.js
+++ web_server/public/javascripts/weapon_authorisation_module.js
@@ -12,10 +12,7 @@ function WeaponAuthorisationModule() {
 		//weapons buttons div
 		this.div1 = document.createElement('div');
 		var weapons_buttons = ['1', '2', '3', '4'];
-		//this.weapons_buttons = [];
 		for (var i = 0; i < weapons_buttons.length; i++){
-			//this.weapons_buttons.push(this.generateButton(weapons_buttons_names[i]);
-			//this.div1.appendChild(this.weapons_buttons[i]);
 			this.div1.appendChild(this.generateWeaponsButtons(weapons_buttons[i]));
 		}
 		
