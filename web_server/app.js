f94634e02c7f0f7b78433b48a3c8e9921d47c722
Jared Linklater <jaredlinklater@gmail.com>
2019-05-30 20:23:34 +0930

    integrating socket.io

--- web_server/app.js
+++ web_server/app.js
@@ -9,6 +9,19 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Socket.io
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
