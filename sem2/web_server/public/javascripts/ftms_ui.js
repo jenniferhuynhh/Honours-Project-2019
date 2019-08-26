1f7c7462c7a58634c763e73c9abd5d5d0a2f0dc6
jennifer huynh <a1709507@student.adelaide.edu.au>
2019-08-26 18:06:50 +0930

    refactored ftms_ui

--- sem2/web_server/public/javascripts/ftms_ui.js
+++ sem2/web_server/public/javascripts/ftms_ui.js
@@ -1,47 +1,50 @@
-function FTMS_UI() {
-	this.window_manager;
-	this.simulator;
-	this.map_module;
-	this.track_table_module;
-	this.classification_module;
-	this.weapon_authorisation_module;
-	this.alert_module;
-	this.messaging_module;
+var FTMS_UI = (function(){
+	//public
+	return {
+		window_manager: null,
+		simulator: null,
+		map_module: null,
+		track_table_module: null,
+		classification_module: null,
+		weapon_authorisation_module: null,
+		alert_module: null,
+		messaging_module: null,
 
-	this.initialise = function() {
-		this.window_manager = WindowManager;
-		this.window_manager.initialise(this);
+		initialise: function(){
+			this.window_manager = WindowManager;
+			this.window_manager.initialise(this);
 
-		this.simulator = new Simulator();
-		this.simulator.initialise(this);
+			this.simulator = new Simulator();
+			this.simulator.initialise(this);
 
-		this.map_module = MapModule;
-		this.map_module.initialise(this);
+			this.map_module = MapModule;
+			this.map_module.initialise(this);
 
-		this.track_table_module = new TrackTableModule();
-		this.track_table_module.initialise(this);
+			this.track_table_module = new TrackTableModule();
+			this.track_table_module.initialise(this);
 
-		this.classification_module = ClassificationModule;
-		this.classification_module.initialise(this);
+			this.classification_module = ClassificationModule;
+			this.classification_module.initialise(this);
 
-		this.weapon_authorisation_module = WeaponAuthorisationModule;
-		this.weapon_authorisation_module.initialise(this);
+			this.weapon_authorisation_module = WeaponAuthorisationModule;
+			this.weapon_authorisation_module.initialise(this);
 
-		this.weapon_firing_module = WeaponFiringModule;
-		this.weapon_firing_module.initialise(this);
+			this.weapon_firing_module = WeaponFiringModule;
+			this.weapon_firing_module.initialise(this);
 
-		this.alert_module = AlertModule;
-		this.alert_module.initialise(this);
+			this.alert_module = AlertModule;
+			this.alert_module.initialise(this);
 
-		this.messaging_module = MessagingModule;
-		this.messaging_module.initialise(this);
+			this.messaging_module = MessagingModule;
+			this.messaging_module.initialise(this);
 
-		this.window_manager.showAll();
+			this.window_manager.showAll();
 
-		this.simulator.test();
+			this.simulator.test();
+		},
+
+		run: function(){
+			this.simulator.run();
+		}
 	}
-
-	this.run = function() {
-		this.simulator.run();
-	}
-}
+}());
--- sem2/web_server/public/javascripts/main.js
+++ sem2/web_server/public/javascripts/main.js
@@ -1,5 +1,5 @@
 function load_ftms_ui() {
-	var ftms_ui = new FTMS_UI();
+	var ftms_ui = FTMS_UI;
 	ftms_ui.initialise();
 	ftms_ui.run();
 }
