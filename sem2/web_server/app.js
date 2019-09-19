cbe49ce761ea576f91d5cf533f0adb37a346af9d
Jared Linklater <jaredlinklater@gmail.com>
2019-09-20 08:09:41 +0930

    created implementations() in app.js which is called after all initial, asynchronous componenets are setup

--- sem2/web_server/app.js
+++ sem2/web_server/app.js
@@ -22,139 +22,70 @@ app.use(session({
 	saveUninitialized: false
 }));
 
-//Kafka setup, implementation below
-var topics = ['tdn-systrk', 'tdn-alert', 'tdn-ui-changes'];
-var kafkaClient, kafkaConsumer, kafkaProducer;
-try {
-	//Converts topic array into objects required for Kafka setup
-	var topicObjects = [];
-	topics.forEach(function(el){topicObjects.push({topic: el});});
-
-	//CLIENT SETUP
-	//var offsetClient = new kafka.KafkaClient();
-	kafkaClient = new kafka.KafkaClient();
-	kafkaClient.createTopics(topics, function(err) {
-		if(err) console.log(err);
-	});
-	
-	// Gets metadata of topics in an attempt to create them
-	/*kafkaClient.loadMetadataForTopics(topics, function(err, res) {
-		if(err) {
-			console.log("Error getting metadata of topics");
-			return console.log(err);
-		}
-	});
-
-	var offset = new kafka.Offset(kafkaClient);
-	offset.fetchLatestOffsets(topics, function(err, offset) {
-		if(err) {
-			console.log('Error Getting Kafka Offsets');
-			return console.log(err);
-		}
-		topicObjects = []; topics.forEach(function(el){topicObjects.push({topic: el, offset: offset[el][0]});});
-	});*/
-
-	//CONSUMER SETUP
-	kafkaConsumer = new kafka.Consumer(
-		kafkaClient,
-		topicObjects, {
-			autoCommit: true,
-			fetchMaxWaitMs: 1000,
-			fetchMaxBytes: 1024 * 1024,
-			encoding: 'buffer',
-			fromOffset: false
-		}
-	);
-	kafkaConsumer.on('error', function(err) {
-		console.log('error', err);
-	});
-
-	//PRODUCER SETUP
-	//kafkaProducer = new kafka.Producer(kafkaClient);
-} catch(e) {
-	console.log(e);
-}
-
 //Socket.io setup
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-var requestCount = 0;
-//Socket.io implementations
-io.on('connection', function(socket) {
-	var address = socket.handshake.address.split(":").pop(); //Gets client's public IP address
-	log(address + ' connected');
+//Kafka and protobuf setups
+var topics = ['tdn-systrk', 'tdn-alert', 'tdn-ui-changes'];
+var kafkaClient, kafkaConsumer, kafkaProducer, proto;
+try {
+	//CLIENT SETUP
+	kafkaClient = new kafka.KafkaClient();
+	kafkaClient.createTopics(topics, function(err, res) {
+		if(err) console.log('Error creating topics', err);
 
-	//MESSAGING MODULE
-	//Send user connect message
-	socket.on('username', function(username, role) {
-		socket.username = username;
-		socket.role = role;
-		socket.join(socket.role);
-		io.emit('is_online', socket.username);
-	});
+		//CONSUMER SETUP
+		var offset = new kafka.Offset(kafkaClient);
+		offset.fetchLatestOffsets(topics, function(err, offset) {
+			if(err) console.log('Error fetching offsets', err);
 
-	//Send user disconnect message
-	socket.on('disconnect', function(username) {
-		io.emit('is_offline', socket.username);
-		var disconnect_address = socket.handshake.address.split(":").pop(); //Gets client's public IP address
-		log(disconnect_address + ' disconnected');
-	});
+			//Converts topic array into array of objects required for Kafka setup
+			var topicObjects = [];
+			topics.forEach(function(el){topicObjects.push({topic: el, offset: offset[el][0]});});
 
-	//Send new chat message
-	socket.on('chat_message', function(message) {
-		io.emit('chat_message', socket.username, message);
-	});
+			kafkaConsumer = new kafka.Consumer(
+				kafkaClient,
+				topicObjects, {
+					autoCommit: true,
+					fetchMaxWaitMs: 1000,
+					fetchMaxBytes: 1024 * 1024,
+					encoding: 'buffer',
+					fromOffset: true
+				}
+			);
+			kafkaConsumer.on('error', function(err) {
+				console.log('error', err);
+			});
 
-	//CLASSIFICATION MODULE
-	//Send updated track information (with Andy's backend ready)
-	/*socket.on('send_track_update', function(track, updatedData) {
-		var payload = [{
-			topic: 'tdn-ui-changes',
-			messages: JSON.stringify(track),
-			partition: 0
-		}];
-		kafkaProducer.send(payload, function(err, data) {});
-	});*/
-	//Send updated track information (without Andy's backend, using sysTracksUpdates collection)
-	socket.on('send_track_update', function(track, updatedData) {
-		updatedData.trackId = track.trackId;
-		Track.updateOne({trackId: track.trackId}, updatedData, function(error, writeOpResult) {
-			if(!writeOpResult.nModified) {
-				Track.create(updatedData, function(err, user) {
-					if(err) return console.log(err);
-				});
-			}
-			io.emit('recieve_track_update', JSON.stringify(track));
+			//PRODUCER SETUP
+			//kafkaProducer = new kafka.Producer(kafkaClient);
+
+			//PROTOBUF SETUP
+			protobuf.load("tdn.proto", function(err, root) {
+				proto = {
+					track: root.lookup("tdn.SystemTrack"),
+					alert: root.lookup("tdn.Alert")
+				};
+
+				//Setups are complete, components are ready; begin implementations
+				implementations();
+			});
 		});
 	});
+} catch(e) {
+	console.log(e);
+}
 
-	//Weapon Authorisation 
-	socket.on('send_request', function(data){
-		data.requestId = requestCount++;
-		data.status = "Pending...";
-		io.to('wo').emit('receive_request', data);
-		io.to('fs').emit('receive_confirmation', data);
-		//io.emit('receive_request', data);
-		//io.emit('receive_confirmation', data);
-	});
-
-	socket.on('send_request_status', function(data){
-		io.emit('receive_request_status', data);
-	});
-});
-
-//Kafka consumer + protobuf implementation
-protobuf.load("tdn.proto", function(err, root) {
-	var proto = {
-		track: root.lookup("tdn.SystemTrack"),
-		alert: root.lookup("tdn.Alert")
-	};
-
+//Called after component's (Kafka, protobuf) setups are complete. Contains implementations of these components.
+function implementations() {
+	//Consumer implementation
 	kafkaConsumer.on('message', async function(message) {
-		if(message.topic == "tdn-systrk") {
+		if(message.topic == "tdn-systrk") {	//Handles incoming tracks
 			var track = proto.track.decode(message.value);
-			Track.findOne({trackId: track.trackId}, function(error, found_track) {
+			track = proto.track.toObject(track, {enums: String, longs: String});
+
+			Track.findOne({trackId: track.trackId}, function(err, found_track) {
 				if(err) return console.log(err);
 
 				if(found_track) { //If changes to track found, overlay those changes before sending to clients
@@ -177,13 +108,78 @@ protobuf.load("tdn.proto", function(err, root) {
 				}
 				io.emit('recieve_track_update', track);
 			});
-		} else if(message.topic == "tdn-alert") {
+		} else if(message.topic == "tdn-alert") { //Handles incoming alerts
 			//var alert = proto.alert.decode(message.value);
 			var alert = JSON.parse(message.value);
 			io.emit('alert', alert);
 		}
 	});
-});
+
+	var requestCount = 0;
+	//Socket.io implementations
+	io.on('connection', function(socket) {
+		var address = socket.handshake.address.split(":").pop(); //Gets client's public IP address
+		log(address + ' connected');
+
+		//MESSAGING MODULE
+		//Send user connect message
+		socket.on('username', function(username, role) {
+			socket.username = username;
+			socket.role = role;
+			socket.join(socket.role);
+			io.emit('is_online', socket.username);
+		});
+
+		//Send user disconnect message
+		socket.on('disconnect', function(username) {
+			io.emit('is_offline', socket.username);
+			var disconnect_address = socket.handshake.address.split(":").pop(); //Gets client's public IP address
+			log(disconnect_address + ' disconnected');
+		});
+
+		//Send new chat message
+		socket.on('chat_message', function(message) {
+			io.emit('chat_message', socket.username, message);
+		});
+
+		//CLASSIFICATION MODULE
+		//Send updated track information (with Andy's backend ready)
+		/*socket.on('send_track_update', function(track, updatedData) {
+			var payload = [{
+				topic: 'tdn-ui-changes',
+				messages: JSON.stringify(track), //probably need protobuf encode
+				partition: 0
+			}];
+			kafkaProducer.send(payload, function(err, data) {});
+		});*/
+		//Send updated track information (without Andy's backend, using sysTracksUpdates collection)
+		socket.on('send_track_update', function(track, updatedData) {
+			updatedData.trackId = track.trackId;
+			Track.updateOne({trackId: track.trackId}, updatedData, function(error, writeOpResult) {
+				if(!writeOpResult.nModified) {
+					Track.create(updatedData, function(err, user) {
+						if(err) return console.log(err);
+					});
+				}
+				io.emit('recieve_track_update', track);
+			});
+		});
+
+		//WEAPON AUTHORISATION
+		socket.on('send_request', function(data){
+			data.requestId = requestCount++;
+			data.status = "Pending...";
+			io.to('wo').emit('receive_request', data);
+			io.to('fs').emit('receive_confirmation', data);
+			//io.emit('receive_request', data);
+			//io.emit('receive_confirmation', data);
+		});
+
+		socket.on('send_request_status', function(data){
+			io.emit('receive_request_status', data);
+		});
+	});
+}
 
 
 //Initiate socket.io server
