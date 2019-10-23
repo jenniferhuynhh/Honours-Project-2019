465f208a1121a6eb41e0db5bb411031b963535ab
Jared Linklater <jaredlinklater@gmail.com>
2019-10-24 08:30:48 +1030

    implemented ownship_id selecting; made header more efficient

--- sem2/web_server/ui/public/javascripts/header.js
+++ sem2/web_server/ui/public/javascripts/header.js
@@ -3,7 +3,7 @@ var Header = (function() {
 	var ftms_ui;
 	var display;
 	var header_cells = {};
-	var ownship_active = false;
+	var ownship;
 
 	//Public
 	return {
@@ -12,7 +12,7 @@ var Header = (function() {
 			ftms_ui = ftms;
 
 			display = document.createElement("table");
-			display.setAttribute("class", "header_table");
+			display.classList.add("header_table");
 
 			//Create header table
 			var header_cells_names = ["course", "speed", "lat", "long", "date", "time", "role", "logout"];
@@ -22,7 +22,6 @@ var Header = (function() {
 				header_row.appendChild(th);
 				header_cells[header_cells_names[i]] = th; //Make cells accessible later
 			}
-			this.clear();
 
 			//Get user's role
 			var role;
@@ -38,58 +37,67 @@ var Header = (function() {
 
 			//Make logout link
 			var logout_link = document.createElement("a");
-			logout_link.setAttribute("href", "logout/");
+			logout_link.href = "logout/";
 			logout_link.innerHTML = "Logout";
 			header_cells["logout"].appendChild(logout_link);
 			display.appendChild(header_row);
 
 			document.getElementById("header").appendChild(display);
 
-			this.update();
-			this.updateLoop();
+			//If ownship id is changed, detach old ownship and look for new one
+			ftms_ui.settings_manager.addEventListener("ownship_id", () => {if(ownship) this.newOwnship()});
+
+			this.updateTimeDate();
+			this.newOwnship();
 		},
 
 		//Updates the header
 		update: function() {
-			var t = new Date();
-			header_cells["date"].innerHTML = t.toLocaleDateString('en-AU');
-			header_cells["time"].innerHTML = t.toLocaleTimeString('en-US');
+			try {
+				header_cells["course"].innerHTML = "Course: " + ownship.course.toFixed(3) + "°";
+				header_cells["speed"].innerHTML = "Speed: " + ownship.speed.toFixed(3) + " knots";
+				header_cells["lat"].innerHTML = "Lat: " + ownship.latitude.toFixed(8);
+				header_cells["long"].innerHTML = "Long: " + ownship.longitude.toFixed(8);
+			} catch(err) {}
+		},
 
-			//Register header as track listener
-			if(!ownship_active) {
-				var ownship = ftms_ui.track_manager.getTrack(ftms_ui.settings_manager.getSetting("ownship_id"));
-				if(ownship) { //If own is currently shown
-					ownship_active = true;
-					ownship.addEventListener("update", function() {
-						header_cells["course"].innerHTML = "Course: " + ownship.course.toFixed(3) + "°";
-						header_cells["speed"].innerHTML = "Speed: " + ownship.speed.toFixed(3) + " knots";
-						header_cells["lat"].innerHTML = "Lat: " + ownship.latitude.toFixed(8);
-						header_cells["long"].innerHTML = "Long: " + ownship.longitude.toFixed(8);
-					});
-
-					ownship.addEventListener("delete", function() {
-						ownship_active = false;
-						this.clear();
-					});
-				}
+		//Looks for ownship and attaches relevant listeners
+		lookForOwnship() {
+			ownship = ftms_ui.track_manager.getTrack(ftms_ui.settings_manager.getSetting("ownship_id"));
+			if(ownship) {
+				this.update();
+				ownship.addEventListener("update", () => this.update());
+				ownship.addEventListener("delete", () => this.clear());
+			} else { //If not found, keep looking
+				setTimeout(() => this.lookForOwnship(), 200);  //Run every 0.5 seconds
 			}
 		},
 
-		//Updates the header every second
-		updateLoop: function() {
-			var self = this;
-			setTimeout(function() {
-				self.update()
-				self.updateLoop();
-			}, 1000);
-		},
-
-		//Clears track fields
+		//Clears ownship and track fields
 		clear: function() {
+			if(ownship) {
+				ownship.removeEventListener("update", this.update);
+				ownship.removeEventListener("delete", this.clear);
+				ownship = null;
+			}
 			header_cells["course"].innerHTML = "Course:";
 			header_cells["speed"].innerHTML = "Speed:";
 			header_cells["lat"].innerHTML = "Lat:";
 			header_cells["long"].innerHTML = "Long:";
+		},
+
+		//Prepares for a new ownship
+		newOwnship: function() {
+			this.clear();
+			this.lookForOwnship();
+		},
+
+		//Updates time and date
+		updateTimeDate: function() {
+			var t = new Date();
+			header_cells["date"].innerHTML = t.toLocaleDateString('en-AU');
+			header_cells["time"].innerHTML = t.toLocaleTimeString('en-US');
+			setTimeout(() => this.updateTimeDate(), 1000); //Run every 1 second
 		}
 	}
 }());
